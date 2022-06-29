d21439d24dc1caefacd3b4f133a00a3dc4d1b6a2
Julian <julian_delisio@hotmail.com>
2022-06-29 19:14:55 -0300

    Carga de items

--- pages/store/index.js
+++ pages/store/index.js
@@ -1,11 +1,28 @@
 import Link from "next/link"
 import Layout from "../../components/layouts"
+import { getItems } from "../../services/itemService";
+import Image from "next/image"
 
 
-export default function index(){
+export default function index({items}){
     return (
         <Layout>
             <h1>Store</h1>
+            {
+                items && items.map(item => <div key={item.id}>
+                <Image src={item.image} width="200" height={200} alt={item.title} />
+                {item.title}</div>)
+            }
         </Layout>
-    )
+    );
+}
+
+export async function getStaticProps(){
+    const res = await getItems()
+
+    return {
+        props: {
+            items: res,
+        },
+    };
 }
